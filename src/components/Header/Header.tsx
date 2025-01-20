2c040539841043be5cbc6ed3fa2736f22d6659d3
LaraBerenguer <laraberenguer@outlook.com>
2025-01-20 16:56:13 +0100

    Updated header layout.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,25 +6,26 @@
 import { Link } from "react-router-dom";
 import Socials from "../Socials";
 
-const Header =() => {
+const Header = () => {
     return (
-        <>
-            <div className="header dark:bg-black flex flex-row justify-between dark:text-white pb-4 px-8">
-                <div className="header-socials content-center m-4">
-                    <Socials />                    
+        <div className="header dark:bg-black flex flex-row justify-center dark:text-white pb-4 px-8">
+            <div className="header-container flex w-full">
+                <div className="header-socials content-center m-4 basis-1/4 flex justify-start items-center">
+                    <Socials />
                 </div>
-                <div className="header-logo">
-                    <div className="header-logo-container m-4">
+                <div className="header-logo m-4 basis-2/4 flex justify-center">
+                    <div className="header-logo-container">
                         <img src=".././logo.png" className="logo h-20"></img>
                     </div>
                 </div>
-                <div className="header-login content-center m-4">
+                <div className="header-login content-center m-4 basis-1/4 flex justify-end items-center">
                     <div className="header-login-container">
                         <Link to="/signup" className="dark:text-grey-500">LOG IN</Link>
                     </div>
                 </div>
             </div>
-        </>
+
+        </div>
     )
 };
 
