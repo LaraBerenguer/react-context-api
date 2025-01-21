29bf2637e84456888f5a5a05514858d505aa377f
LaraBerenguer <laraberenguer@outlook.com>
2025-01-21 18:23:58 +0100

    Added navigation button to HomePage.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import Carousel from "../components/Carousel";
+import { Button } from "../components/Button/Button";
 
 const HomePage = () => {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/starships")
+    };
+
     return (
-        <Carousel />
+        <div className="homepage">
+            <div className="homepage-carousel-container">
+                <Carousel />
+            </div>
+            <div className="homepage-button flex justify-center p-10">
+                <Button label={"GO TO STARSHIPS"} size={"large"} primary={true} backgroundColor={"#ffe91f"} color={"##000"} onClick={handleClick} />
+            </div>
+        </div>
     )
 };
 
